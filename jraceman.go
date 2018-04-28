d59b1b7c21cc27d02e6a50e68ba89b1096527682
Jim McBeath <git@j.jimmc.org>
2018-04-28 05:04:59 +0000

    Add sqlite3 db driver, add -db and -create command line options.

--- jraceman.go
+++ jraceman.go
@@ -12,12 +12,15 @@ import (
   "github.com/jimmc/jracemango/dbrepo"
 
   // _ "github.com/go-sql-driver/mysql"
-  _ "github.com/proullon/ramsql/driver"
+  _ "github.com/proullon/ramsql/driver" // driver name: ramsql
+  _ "github.com/mattn/go-sqlite3"       // driver name: sqlite3
 )
 
 type config struct {
   port int
   uiRoot string
+  db string
+  create bool
 }
 
 func main() {
@@ -25,21 +28,27 @@ func main() {
 
   flag.IntVar(&config.port, "port", 8080, "port on which to listen for connections")
   flag.StringVar(&config.uiRoot, "uiroot", "", "location of ui root (build/default)")
+  flag.StringVar(&config.db, "db", "", "location of database, in the form driver:name")
+  flag.BoolVar(&config.create, "create", false, "true to create the database specified by -db")
 
   flag.Parse()
 
-  repoPath := "ramsql:TestDatabase"
-  // repoPath := "mysql:user:password@tcp(127.0.0.1:3306)/hello"
-  dbRepos, err := dbrepo.Open(repoPath)
+  if config.db == "" {
+    log.Fatal("-db is required")
+  }
+
+  dbRepos, err := dbrepo.Open(config.db)
   if err != nil {
     log.Fatalf("Failed to open repository: %v", err)
   }
   defer dbRepos.Close()
 
-  // TODO - for testing, with ramsql, create tables
-  err = dbRepos.CreateTables()
-  if err != nil {
-    log.Fatalf("Failed to create repository tables: %v", err)
+  if config.create {
+    log.Printf("Creating database tables")
+    err = dbRepos.CreateTables()
+    if err != nil {
+      log.Fatalf("Failed to create repository tables: %v", err)
+    }
   }
 
   ph := app.Placeholder{}       // Just to use the app package
