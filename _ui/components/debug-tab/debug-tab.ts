b0f5f491930c8622734f61fd0030dba0ca63cd62
Jim McBeath <git@j.jimmc.org>
2018-05-21 16:41:55 -0700

    Work on layout.

--- _ui/components/debug-tab/debug-tab.ts
+++ _ui/components/debug-tab/debug-tab.ts
@@ -1,4 +1,6 @@
 @Polymer.decorators.customElement('debug-tab')
 class DebugTab extends Polymer.Element {
 
+  @Polymer.decorators.property({type: Number})
+  selectedTab: number = 0;
 }
--- _ui/components/jraceman-app/jraceman-app.ts
+++ _ui/components/jraceman-app/jraceman-app.ts
@@ -3,4 +3,9 @@
 @Polymer.decorators.customElement('jraceman-app')
 class JRacemanApp extends Polymer.Element {
 
+  @Polymer.decorators.property({type: Number})
+  selectedTopTab: number = 0;
+
+  @Polymer.decorators.property({type: Number})
+  selectedBottomTab: number = 0;
 }
