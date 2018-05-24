443f5ece272d817019f302a88918216a9fd39697
Jim McBeath <git@j.jimmc.org>
2018-05-23 20:43:50 -0700

    Add Sport Setup, Competition Setup, with empty Query tab.

---
+++ _ui/components/competitions-setup/competitions-setup.ts
@@ -0,0 +1,6 @@
+@Polymer.decorators.customElement('competitions-setup')
+class CompetitionsSetup extends Polymer.Element {
+
+  @Polymer.decorators.property({type: Number})
+  selectedTab: number = 0;
+}
---
+++ _ui/components/sport-setup/sport-setup.ts
@@ -0,0 +1,6 @@
+@Polymer.decorators.customElement('sport-setup')
+class SportSetup extends Polymer.Element {
+
+  @Polymer.decorators.property({type: Number})
+  selectedTab: number = 0;
+}
