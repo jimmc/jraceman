ef534dbcb7a9d872dbe03ffd9756c9ad65bbd3ed
Jim McBeath <git@j.jimmc.org>
2018-06-23 21:41:25 -0700

    Implement UI for upgrade.

--- _ui/components/database-menu/database-menu.ts
+++ _ui/components/database-menu/database-menu.ts
@@ -14,10 +14,59 @@ class DatabaseMenu extends Polymer.Element {
   }
 
   checkUpgradeClicked() {
-    ClientMessages.append("database", "Check Upgrade is not yet implemented");
+    this.doUpgrade(true)
   }
 
   upgradeClicked() {
-    ClientMessages.append("database", "Upgrade is not yet implemented");
+    this.doUpgrade(false)
+  }
+
+  async doUpgrade(dryrun: boolean) {
+    const dryrunFlag = (dryrun ? '?dryrun=true' : '');
+    const resultPrefix = (dryrun ? "Would execute: " : "Executed: ");
+    const path = '/api/database/upgrade' + dryrunFlag;
+    let sections : string[];
+    try {
+      sections = await ApiManager.xhrJson(path);
+    } catch (e) {
+      console.error(e);
+      ClientMessages.append("database", "Error getting list of sections: " + e.responseText);
+      return;
+    }
+    const op = (dryrun ? "Upgrade checking " : "Upgrading ");
+    ClientMessages.append("database", op + sections.length + " sections")
+    let numSectionsToUpgrade = 0;
+    for (let i = 0; i < sections.length; i++) {
+      const section = sections[i];
+      const sectionPath = '/api/database/upgrade/' + section + dryrunFlag;
+      const options : XhrOptions = {
+        method: 'POST',
+      };
+      // ClientMessages.append("database", "Updating " + section);
+      let sectionResult;
+      try {
+        sectionResult = await ApiManager.xhrJson(sectionPath, options);
+      } catch (e) {
+        console.error("section error ", e);
+        ClientMessages.append("database", "Error in section " + section + ": " + e.responseText);
+        if (!dryrun) {
+          // If we are not doing a dryrun, stop after an error.
+          return;
+        } else {
+          continue;
+        }
+      }
+      if (sectionResult.Nop) {
+        // ClientMessages.append("database", "No change to " + section);
+      } else {
+        ClientMessages.append("database", resultPrefix + sectionResult.Message);
+        numSectionsToUpgrade++;
+      }
+    }
+    if (numSectionsToUpgrade == 0) {
+      ClientMessages.append("database", "Upgrade done, no changes");
+    } else {
+      ClientMessages.append("database", "Upgrade done");
+    }
   }
 }
