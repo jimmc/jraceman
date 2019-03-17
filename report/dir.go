78d3bc755002b17c2ba85161659f0d4b325b2ec8
Jim McBeath <git@j.jimmc.org>
2019-03-17 08:16:51 -0700

    Add support for reading template attributes from multiple dirs.

--- report/dir.go
+++ report/dir.go
@@ -6,6 +6,52 @@ import (
   "github.com/jimmc/gtrepgen/gen"
 )
 
+type ReportAttributes struct {
+  Name string
+  Display string
+}
+
+/* ClientVisibleReports returns the list of reports and their attributes
+ * that should be visible to a client.
+ * Once we have user ids and authorizations, this function should accept
+ * a user id and return only data that should be visible to that user.
+ */
+func ClientVisibleReports(reportRoots []string) ([]*ReportAttributes, error) {
+  allAttrs := make([]*ReportAttributes, 0)
+  for _, root := range reportRoots {
+    attrs, err := ClientVisibleReportsOne(root)
+    if err != nil {
+      return nil, err
+    }
+    allAttrs = append(allAttrs, attrs...)
+  }
+  return allAttrs, nil
+}
+
+func ClientVisibleReportsOne(templateDir string) ([]*ReportAttributes, error) {
+  attrs, err := ReadTemplateAttrs(templateDir)
+  if err != nil {
+    return nil, err
+  }
+  reports := make([]*ReportAttributes, 0)
+  for _, tplAttrs := range attrs {
+    name, ok := tplAttrs["name"].(string)
+    if !ok {
+      continue  // Name is not a string value, ignore this entry
+    }
+    display, ok := tplAttrs["display"].(string)
+    if !ok {
+      continue  // Display is not a string value, ignore this entry
+    }
+    report := &ReportAttributes{
+      Name: name,
+      Display: display,
+    }
+    reports = append(reports, report)
+  }
+  return reports, nil
+}
+
 /* ReadTemplateAttrs loads the attributes from all the template files in
  * the given directory.
  */
@@ -23,6 +69,7 @@ func ReadTemplateAttrs(templateDir string) ([]map[string]interface{}, error) {
     if !ok {
       return nil, fmt.Errorf("invalid data type for template %q", fattrs.Name)
     }
+    fmap["name"] = fattrs.Name
     attrMaps = append(attrMaps, fmap)
   }
   return attrMaps, err
--- report/dir_test.go
+++ report/dir_test.go
@@ -1,8 +1,26 @@
 package report
 
 import (
+  "fmt"
   "testing"
+
+  "github.com/google/go-cmp/cmp"
 )
+
+func TestClientVisibleReports(t *testing.T) {
+  reports, err := ClientVisibleReportsOne("template")
+  if err != nil {
+    t.Fatalf("ClientVisibleReports error: %v", err)
+  }
+  expected := []*ReportAttributes{
+    {Name: "org.jimmc.jraceman.Entries", Display: "Entries" },
+    {Name: "org.jimmc.jraceman.Lanes", Display: "Lanes" },
+  }
+  got, want := reports, expected
+  if diff := cmp.Diff(want, got); diff != "" {
+    t.Errorf("ClientVisibleReports() mismatch (-want +got):\n%s", diff)
+  }
+}
 
 func TestReadTemplateAttrs(t *testing.T) {
   attrslist, err := ReadTemplateAttrs("template")
@@ -11,5 +29,9 @@ func TestReadTemplateAttrs(t *testing.T) {
   }
   if got, want := len(attrslist), 2; got != want {
     t.Fatalf("Wrong number of files-with-attributes found, got %d, want %d", got, want)
+  }
+  fmt.Printf("attrslist: %+v\n", attrslist)
+  if got, want := attrslist[0]["name"], "org.jimmc.jraceman.Entries"; got != want {
+    t.Errorf("Name of first report: got %s, want %s", got, want)
   }
 }
