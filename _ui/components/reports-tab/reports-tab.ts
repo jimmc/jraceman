de7de0d4e08784030db2157451ac2e29747c8311
Jim McBeath <git@j.jimmc.org>
2019-03-30 20:38:40 -0700

    Add report order-by choice to UI.

--- _ui/components/reports-tab/reports-tab.ts
+++ _ui/components/reports-tab/reports-tab.ts
@@ -1,15 +1,32 @@
+// ReportAttributes is what we get from the API for each report.
+interface ReportAttributes {
+  Name: string;
+  Display: string;
+  OrderBy: {[key:string]:string}[];
+}
+
+// OrderBy is what we prepare for our UI for each OrderBy.
+interface OrderBy {
+  Name: string;
+  Display: string;
+}
+
 @Polymer.decorators.customElement('reports-tab')
 class ReportsTab extends Polymer.Element {
 
   @Polymer.decorators.property({type: Object, notify: true})
-  reportList: object;
+  orderByList: OrderBy[];
+
+  @Polymer.decorators.property({type: Object, notify: true})
+  reportList: ReportAttributes[];
 
   @Polymer.decorators.property({type: Object, notify: true})
   reportResults: object;
 
-  ready() {
+  async ready() {
     super.ready()
-    this.loadReportList()
+    await this.loadReportList()
+    this.reportNameChanged()
   }
 
   async loadReportList() {
@@ -24,21 +41,53 @@ class ReportsTab extends Polymer.Element {
     }
   }
 
+  reportNameChanged() {
+    const reportName = this.$.main.querySelector("#reportName").value;
+    this.updateOrderByList(reportName)
+  }
+
+  updateOrderByList(reportName: string) {
+    var obl: OrderBy[] = []
+    const reportAttrs = this.findReport(reportName)
+    const reportOrderBys: {[key:string]:string}[] = reportAttrs.OrderBy
+    if (reportOrderBys) {
+      for (let item of reportOrderBys) {
+        const orderby: OrderBy = {
+          Name: item["Name"],
+          Display: item["Display"],
+        }
+        obl.push(orderby)
+      }
+    }
+    this.orderByList = obl
+  }
+
+  findReport(reportName: string): ReportAttributes {
+    for (const report of this.reportList) {
+      if (report.Name == reportName) {
+        return report
+      }
+    }
+    throw("Report not found: " + reportName)
+  }
+
   // Generate the report.
   async generate() {
-    console.log("Generate");
-    const reportName = this.$.main.querySelector("#reportName").value;
-    const path = '/api/report/generate/';
+    console.log("Generate")
+    const reportName = this.$.main.querySelector("#reportName").value
+    const orderBy = this.$.main.querySelector("#orderBy").value
+    const path = '/api/report/generate/'
     const formData = {
-      name: reportName
-    };
+      name: reportName,
+      orderby: orderBy
+    }
     const options = {
       method: 'POST',
       params: formData
-    };
+    }
     try {
       const result = await ApiManager.xhrJson(path, options)
-      this.reportResults = result;
+      this.reportResults = result
     } catch(e) {
       this.reportResults = {
         Error: e.responseText
