fe3ae5ab4ae466f5bc124cd1965907ce492f5abe
Jim McBeath <git@j.jimmc.org>
2019-04-12 22:57:30 -0700

    Switch to using a golden file for client-visible-reports.

--- report/controls_test.go
+++ report/controls_test.go
@@ -1,11 +1,13 @@
 package report
 
 import (
+  "encoding/json"
+  "fmt"
+  "os"
   "testing"
 
   "github.com/jimmc/jracemango/dbrepo"
-
-  "github.com/google/go-cmp/cmp"
+  dbtest "github.com/jimmc/jracemango/dbrepo/test"
 )
 
 func TestClientVisibleReports(t *testing.T) {
@@ -18,102 +20,21 @@ func TestClientVisibleReports(t *testing.T) {
   if err != nil {
     t.Fatalf("ClientVisibleReports error: %v", err)
   }
-  keyOps := []string{"eq"}
-  stringOps := []string{"eq", "ne", "gt", "ge", "lt", "le", "like"}
-  dfltOps := []string{"eq", "ne", "gt", "ge", "lt", "le"}
-  expected := []*ReportControls{
-    {
-      Name: "org.jimmc.jraceman.Entries",
-      Display: "Entries",
-      Description: "Entries ordered as selected.",
-      OrderBy: []ControlsOrderByItem{
-        ControlsOrderByItem{Name: "team",        Display: "Team, Person, Event"},
-        ControlsOrderByItem{Name: "person",      Display: "Person, Event"},
-        ControlsOrderByItem{Name: "eventTeam",   Display: "Event, Team, Person"},
-        ControlsOrderByItem{Name: "eventPerson", Display: "Event, Person"},
-      },
-      Where: []ControlsWhereItem{
-	{Name: "event_id", Display: "Event", Ops: keyOps, KeyTable: "event"},
-	{Name: "event_name", Display: "Event Name", Ops: stringOps},
-	{Name: "event_number", Display: "Event Number", Ops: dfltOps},
-	{Name: "team_id", Display: "Team", Ops: keyOps, KeyTable: "team"},
-	{Name: "team_shortname", Display: "Team Short Name", Ops: stringOps},
-	{Name: "team_name", Display: "Team Name", Ops: stringOps},
-	{Name: "person_id", Display: "Person", Ops: keyOps, KeyTable: "person"},
-      },
-    },
-    {
-      Name:        "org.jimmc.jraceman.EntryCountPerEvent",
-      Display:     "Entry Count Per Event",
-      Description: "The number of Entries and Teams entered for each Event.",
-      OrderBy: []ControlsOrderByItem{
-        {Name: "event", Display: "Event"},
-        {Name: "teamcount", Display: "#Teams"},
-        {Name: "entrycount", Display: "#Entries"},
-        {Name: "groupcount", Display: "#Groups"},
-      },
-      Where: []ControlsWhereItem{
-	{Name: "event_id", Display: "Event", Ops: keyOps, KeyTable: "event"},
-	{Name: "event_name", Display: "Event Name", Ops: stringOps},
-	{Name: "event_number", Display: "Event Number", Ops: dfltOps},
-	{Name: "meet_id", Display: "Meet", Ops: keyOps, KeyTable: "meet"},
-	{Name: "person_id", Display: "Person", Ops: keyOps, KeyTable: "person"},
-	{Name: "team_id", Display: "Team", Ops: keyOps, KeyTable: "team"},
-	{Name: "team_shortname", Display: "Team Short Name", Ops: stringOps},
-	{Name: "team_name", Display: "Team Name", Ops: stringOps},
-      },
-    },
-    {
-      Name:        "org.jimmc.jraceman.EntryCountPerLevel",
-      Display:     "Entry Count Per Level",
-      Description: "The number of Entries and Teams entered for each Level.",
-      OrderBy: []ControlsOrderByItem{
-        {Name: "level", Display: "Level"},
-        {Name: "teamcount", Display: "#Teams"},
-        {Name: "personcount", Display: "#People"},
-        {Name: "entrycount", Display: "#Entries"},
-        {Name: "groupcount", Display: "#Groups"},
-      },
-      Where: []ControlsWhereItem{
-	{Name: "event_id", Display: "Event", Ops: keyOps, KeyTable: "event"},
-	{Name: "event_name", Display: "Event Name", Ops: stringOps},
-	{Name: "event_number", Display: "Event Number", Ops: dfltOps},
-	{Name: "meet_id", Display: "Meet", Ops: keyOps, KeyTable: "meet"},
-	{Name: "person_id", Display: "Person", Ops: keyOps, KeyTable: "person"},
-	{Name: "team_id", Display: "Team", Ops: keyOps, KeyTable: "team"},
-	{Name: "team_shortname", Display: "Team Short Name", Ops: stringOps},
-	{Name: "team_name", Display: "Team Name", Ops: stringOps},
-      },
-    },
-    {
-      Name:        "org.jimmc.jraceman.EntryCountPerPerson",
-      Display:     "Entry Count Per Person",
-      Description: "The number of Entries entered for each Person.",
-      OrderBy: []ControlsOrderByItem{
-        {Name: "person", Display: "Person"},
-        {Name: "entrycount", Display: "#Entries"},
-      },
-      Where: []ControlsWhereItem{
-	{Name: "event_id", Display: "Event", Ops: keyOps, KeyTable: "event"},
-	{Name: "event_name", Display: "Event Name", Ops: stringOps},
-	{Name: "event_number", Display: "Event Number", Ops: dfltOps},
-	{Name: "meet_id", Display: "Meet", Ops: keyOps, KeyTable: "meet"},
-	{Name: "person_id", Display: "Person", Ops: keyOps, KeyTable: "person"},
-	{Name: "team_id", Display: "Team", Ops: keyOps, KeyTable: "team"},
-	{Name: "team_shortname", Display: "Team Short Name", Ops: stringOps},
-	{Name: "team_name", Display: "Team Name", Ops: stringOps},
-      },
-    },
-    {
-      Name: "org.jimmc.jraceman.Lanes",
-      Display: "Lanes",
-      Description: "",
-      OrderBy: []ControlsOrderByItem{},
-      Where: []ControlsWhereItem{},
-    },
+  outfile := "testdata/client-visible-reports.out"
+  goldenfile := "testdata/client-visible-reports.golden"
+  f, err := os.Create(outfile)
+  if err != nil {
+    t.Fatalf("Error opening outfile: %v", err)
   }
-  got, want := reports, expected
-  if diff := cmp.Diff(want, got); diff != "" {
-    t.Errorf("ClientVisibleReports() mismatch (-want +got):\n%s", diff)
+  s, err := json.MarshalIndent(reports, "", "\t")
+  if err != nil {
+    t.Fatalf("Error encoding json: %v", err)
+  }
+  fmt.Fprintf(f, "%s", s)
+  if err := f.Close(); err != nil {
+    t.Fatalf("Error closing outfile: %v", err)
+  }
+  if err := dbtest.CompareOutToGolden(outfile, goldenfile); err != nil {
+    t.Fatalf("Error comparing to golden: %v", err)
   }
 }
