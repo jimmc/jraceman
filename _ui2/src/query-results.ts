5099996a7d6ab3e9d13ec0375bd4b0e5f3876fee
Jim McBeath <git@j.jimmc.org>
2022-09-08 21:30:55 -0700

    Replace vaadin-grid by simple table so I can get row-select working.

--- _ui2/src/query-results.ts
+++ _ui2/src/query-results.ts
@@ -1,8 +1,6 @@
 import {LitElement, html, css} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import {repeat} from 'lit/directives/repeat.js';
-import '@vaadin/grid/vaadin-grid.js';
-import '@vaadin/grid/vaadin-grid-column.js';
 
 import { ColumnDesc, QueryResultsData, QueryResultsEvent } from './table-desc.js'
 
@@ -57,38 +55,47 @@ export class QueryResults extends LitElement {
     this.queryResults = evt.detail.results
   }
 
-  // vaadin-grid needs a path within each row item passed to it, so we
-  // convert each row to a map using the column names as field names.
-  queryResultsAsMaps(queryResults: QueryResultsData) {
-    let res = []
-    for (let row of queryResults.Rows) {
-      let rr = {}
-      let cx = 0
-      for (let col of queryResults.Columns) {
-        const cc = col as ColumnDesc
-        // With javascript, we could use rr[cc.Name]=row[cx]
-        // and access that using a field op such as rr.F where F=cc.Name,
-        // but typescript won't let us do that, so we resort to using eval instead.
-        const ev = "rr." + cc.Name + "=JSON.parse('" + JSON.stringify(row[cx]) + "')"
-        eval(ev)        // rr.F = row[cx]   where F=cc.Name
-        cx++
-      }
-      res.push(rr)
+  onClick(e: PointerEvent) {
+    console.log("QueryResult.onClick",e)
+    const td = eval("e.path[0]")
+    const rowIndexStr = td.getAttribute('rowIndex')
+    if (!rowIndexStr) {
+      console.log("no rowIndex in event")
+      return
     }
-    return res
+    const rowIndex = parseInt(rowIndexStr)
+    this.selectRowByIndex(rowIndex)
+  }
+
+  selectRowByIndex(rowIndex: number) {
+    console.log("QueryResults.selectRowByIndex", rowIndex)      // TODO - implement this
   }
 
   render() {
     return html`
       Table: ${this.queryResults.Table}<br/>
       ${this.queryResults.Error}
-        <vaadin-grid id="grid" items="${JSON.stringify(this.queryResultsAsMaps(this.queryResults))}">
+      <table @click="${this.onClick}">
+        <tr>
+          ${/*@ts-ignore*/
+            repeat(this.queryResults.Columns, (col:ColumnDesc/*, colIndex*/) => html`
+            <th>
+              ${col.Name}
+            </th>
+          `)}
+        </tr>
+        ${/*@ts-ignore*/
+          repeat(this.queryResults.Rows, (row:any[], rowIndex) => html`
+          <tr>
           ${/*@ts-ignore*/
             repeat(this.queryResults.Columns, (col:ColumnDesc, colIndex) => html`
-            <vaadin-grid-column header="${col.Name}" path="${col.Name}">
-            </vaadin-grid-column>
+            <td rowIndex=${rowIndex}>
+              ${row[colIndex]}
+            </td>
           `)}
-        </vaadin-grid>
+          </tr>
+        `)}
+      </table>
     `;
   }
 }
