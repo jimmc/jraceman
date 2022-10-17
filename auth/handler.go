fd34328ccbf6846cd3e994c2c504294907d3ccb7
Jim McBeath <jimmc@nwlink.com>
2022-10-17 08:58:52 -0700

    Fix to match a change in auth package

--- auth/handler.go
+++ auth/handler.go
@@ -8,7 +8,7 @@ import (
 
 // NewHandler returns our auth handler, which in turn wraps other
 // handlers when auth is required.
-func NewHandler(db *sql.DB, maxClockSkewSeconds int) authlib.Handler {
+func NewHandler(db *sql.DB, maxClockSkewSeconds int) *authlib.Handler {
   authStore := NewPwDB(db)
   authHandler := authlib.NewHandler(&authlib.Config{
     Prefix: "/auth/",
