ca2d423116c7a026bf382cf7e546fa401927f920
Jim McBeath <git@j.jimmc.org>
2023-03-06 21:48:52 -0800

    Set up testing structure for UpdateRaceInfo

--- dbrepo/eventinfo_test.go
+++ dbrepo/eventinfo_test.go
@@ -1,10 +1,13 @@
 package dbrepo_test
 
 import (
+  "context"
   "encoding/json"
   "io/ioutil"
+  "os"
   "testing"
 
+  "github.com/jimmc/jraceman/domain"
   dbtest "github.com/jimmc/jraceman/dbrepo/test"
 
   goldenbase "github.com/jimmc/golden/base"
@@ -27,15 +30,18 @@ func TestEventRaceInfo(t *testing.T) {
   for _, tt := range tests {
     t.Run(tt.testName, func(t *testing.T) {
 
+      // Load the database.
       setupfilename := "testdata/" + tt.setupName + ".setup"
-
       dbRepos, err := dbtest.ReposAndLoadFile(setupfilename)
       if err != nil {
         t.Fatalf(err.Error())
       }
       defer dbRepos.Close()
 
+      // Run the function under test.
       eventInfo, err := dbRepos.EventInfo().EventRaceInfo(tt.eventId)
+
+      // Check the result.
       if tt.expectError {
         if err == nil {
           t.Fatal("Expected error but did not get one")
@@ -45,6 +51,7 @@ func TestEventRaceInfo(t *testing.T) {
           t.Fatal(err)
         }
 
+        // Write out the database.
         jsonData, err := json.MarshalIndent(eventInfo, "", " ")
         if err != nil {
           t.Fatal(err)
@@ -58,6 +65,73 @@ func TestEventRaceInfo(t *testing.T) {
         if err != nil {
           t.Fatal(err)
         }
+
+        // Check that the result is as expected.
+        goldenfile := "testdata/" + outName + ".golden"
+        if err := goldenbase.CompareOutToGolden(outfile, goldenfile); err != nil {
+          t.Fatal(err)
+        }
+      }
+    })
+  }
+}
+
+func TestUpdateRaceInfo(t *testing.T) {
+  tests := []struct{
+    testName string
+    setupName string
+    outName string
+    expectError bool
+    eventInfo *domain.EventInfo
+    racesToCreate []*domain.RaceInfo
+    racesToDelete []*domain.RaceInfo
+    racesToModFrom []*domain.RaceInfo
+    racesToModTo []*domain.RaceInfo
+  } {
+      { "no changes", "updateraceinfo", "updateraceinfo-nochange", false, nil, nil, nil, nil, nil },
+  }
+  for _, tt := range tests {
+    t.Run(tt.testName, func(t *testing.T) {
+
+      // Load the database.
+      setupfilename := "testdata/" + tt.setupName + ".setup"
+      dbRepos, err := dbtest.ReposAndLoadFile(setupfilename)
+      if err != nil {
+        t.Fatalf(err.Error())
+      }
+      defer dbRepos.Close()
+
+      // Run the function under test.
+      ctx := context.Background()
+      err = dbRepos.EventInfo().UpdateRaceInfo(ctx, tt.eventInfo,
+        tt.racesToCreate, tt.racesToDelete, tt.racesToModFrom, tt.racesToModTo)
+
+      // Check the result.
+      if tt.expectError {
+        if err == nil {
+          t.Fatal("Expected error but did not get one")
+        }
+      } else {
+        if err != nil {
+          t.Fatal(err)
+        }
+
+        // Write out the database.
+        outName := tt.outName
+        if outName=="" {
+          outName = tt.setupName
+        }
+        outfile := "testdata/" + outName + ".out"
+        w, err := os.Create(outfile)
+        if err != nil {
+          t.Fatalf("error opening export output file %s: %v", outfile, err)
+        }
+        if err := dbRepos.Export(w); err != nil {
+          t.Fatalf("error exporting to %s: %v", outfile, err)
+        }
+        w.Close()
+
+        // Check that the result is as expected.
         goldenfile := "testdata/" + outName + ".golden"
         if err := goldenbase.CompareOutToGolden(outfile, goldenfile); err != nil {
           t.Fatal(err)
