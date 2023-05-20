2ce51defdd55c0b64d2f7c18120128582a94dbbc
Jim McBeath <git@j.jimmc.org>
2023-05-19 22:38:08 -0700

    Refactor to extract query-fields out of table-query

---
+++ _ui/src/query-fields.ts
@@ -0,0 +1,126 @@
+import {LitElement, html, css} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import {repeat} from 'lit/directives/repeat.js';
+import {when} from 'lit/directives/when.js';
+
+import { TableDesc } from './table-desc.js'
+
+/**
+ * query-fields provides a set of input fields to collect query parameters.
+ */
+@customElement('query-fields')
+export class QueryFields extends LitElement {
+  static styles = css`
+  `;
+
+  @property({type: Object})
+  tableDesc: TableDesc = {
+    Table: "(unset-in-query-fields)",
+    Columns: [],
+  };
+
+  // getSelectElement gets an HTMLSelectElement by selector.
+  getSelectElement(selector: string) {
+    const shadowRoot = this.shadowRoot
+    if (shadowRoot == null) {
+      console.error("shadowRoot is null")
+      return null
+    }
+    return shadowRoot.querySelector(selector) as HTMLSelectElement
+  }
+
+  // getSelectValue gets the value of a <select> element.
+  getSelectValue(selector: string) {
+    const sel = this.getSelectElement(selector)
+    if (sel == null) {
+      console.error("select element is null in getSelectValue")
+      return null
+    }
+    return sel.value
+  }
+
+  // setSelectValue sets the value of a <select> element.
+  setSelectValue(selector: string, val: string) {
+    const sel = this.getSelectElement(selector)
+    if (sel == null) {
+      console.error("select element is null in setSelectValue")
+      return
+    }
+    sel.value = val
+  }
+
+  clear() {
+    for (let col of this.tableDesc.Columns) {
+      const name = col.Name;
+      this.setSelectValue("#val_"+name, '');
+      this.setSelectValue("#op_"+name, 'eq');
+    }
+  }
+
+  fieldsAsParams() {
+    let params = [];
+    for (let col of this.tableDesc.Columns) {
+      const name = col.Name;
+      const colVal = this.getSelectValue("#val_"+name)
+      const colOp = this.getSelectValue("#op_"+name)
+      console.log("QueryFields.search",name, colOp, colVal)
+      if (colVal && colOp) {
+        const colParams = {
+          name: name,
+          op: colOp,
+          value: colVal,
+        };
+        params.push(colParams);
+      }
+    }
+    return params
+  }
+
+  isStringColumn(colType: string) {
+    return colType == "string";
+  }
+
+  render() {
+    return html`
+      <table>
+        ${repeat(this.tableDesc.Columns, (col /*, colIndex*/) => html`
+          <tr>
+            <td>${col.Label}</td>
+            <td>
+              <select id="op_${col.Name}">
+                <option value="eq">=</option>
+                <option value="ne">!=</option>
+                ${when(!col.FKTable, ()=>html`
+                  <option value="gt">&gt;</option>
+                  <option value="ge">&gt;=</option>
+                  <option value="lt">&lt;</option>
+                  <option value="le">&lt;=</option>
+                  ${when(this.isStringColumn(col.Type), ()=>html`
+                    <option value="like">LIKE</option>
+                  `)}
+                `)}
+              </select>
+            </td>
+            <td>
+              ${when(col.FKTable, ()=>html`
+                <select id="val_${col.Name}">
+                  ${repeat(col.FKItems, (keyitem) => html`
+                    <option value="${keyitem.ID}">${keyitem.Summary}</option>
+                  `)}
+                </select>
+              `, ()=>html`
+                <input type=text id="val_${col.Name}" name=${col.Name} label=${col.Name}></input>
+              `)}
+            </td>
+          </tr>
+          `)}
+      </table>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'query-fields': QueryFields;
+  }
+}
--- _ui/src/table-query.ts
+++ _ui/src/table-query.ts
@@ -1,10 +1,12 @@
-import {LitElement, html, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
-import {repeat} from 'lit/directives/repeat.js';
-import {when} from 'lit/directives/when.js';
+import { LitElement, html, css } from 'lit'
+import { PropertyValues } from 'lit-element'
+import { customElement, property } from 'lit/decorators.js'
+
+import './query-fields.js'
 
 import { ApiManager, XhrOptions } from './api-manager.js'
 import { PostError } from './message-log.js'
+import { QueryFields } from './query-fields.js'
 import { TableDesc, QueryResultsEvent } from './table-desc.js'
 
 /**
@@ -27,6 +29,13 @@ export class TableQuery extends LitElement {
   @property({type: String})
   selectedOp: string = '';
 
+  queryFields?: QueryFields
+
+  firstUpdated(changedProperties:PropertyValues<this>) {
+    super.firstUpdated(changedProperties);
+    this.queryFields = this.shadowRoot!.querySelector("query-fields")! as QueryFields
+  }
+
   // getSelectElement gets an HTMLSelectElement by selector.
   getSelectElement(selector: string) {
     const shadowRoot = this.shadowRoot
@@ -37,51 +46,13 @@ export class TableQuery extends LitElement {
     return shadowRoot.querySelector(selector) as HTMLSelectElement
   }
 
-  // getSelectValue gets the value of a <select> element.
-  getSelectValue(selector: string) {
-    const sel = this.getSelectElement(selector)
-    if (sel == null) {
-      console.error("select element is null in getSelectValue")
-      return null
-    }
-    return sel.value
-  }
-
-  // setSelectValue sets the value of a <select> element.
-  setSelectValue(selector: string, val: string) {
-    const sel = this.getSelectElement(selector)
-    if (sel == null) {
-      console.error("select element is null in setSelectValue")
-      return
-    }
-    sel.value = val
-  }
-
   clear() {
-    for (let col of this.tableDesc['Columns']) {
-      const name = col.Name;
-      this.setSelectValue("#val_"+name, '');
-      this.setSelectValue("#op_"+name, 'eq');
-    }
+    this.queryFields!.clear()
   }
 
   async search() {
     console.log("TableQuery.search begin");
-    let params = [];
-    for (let col of this.tableDesc['Columns']) {
-      const name = col.Name;
-      const colVal = this.getSelectValue("#val_"+name)
-      const colOp = this.getSelectValue("#op_"+name)
-      console.log("TableQuery.search",name, colOp, colVal)
-      if (colVal && colOp) {
-        const colParams = {
-          name: name,
-          op: colOp,
-          value: colVal,
-        };
-        params.push(colParams);
-      }
-    }
+    const params = this.queryFields!.fieldsAsParams()
     const options: XhrOptions = {
       method: "POST",
       params: params,
@@ -120,40 +91,8 @@ export class TableQuery extends LitElement {
     return html`
         <form>
           <button type=button @click="${this.search}">Search</button>
-          <button  type=button @click="${this.clear}">Clear</button>
-          <table>
-            ${repeat(this.tableDesc.Columns, (col /*, colIndex*/) => html`
-              <tr>
-                <td>${col.Label}</td>
-                <td>
-                  <select id="op_${col.Name}">
-                    <option value="eq">=</option>
-                    <option value="ne">!=</option>
-                    ${when(!col.FKTable, ()=>html`
-                      <option value="gt">&gt;</option>
-                      <option value="ge">&gt;=</option>
-                      <option value="lt">&lt;</option>
-                      <option value="le">&lt;=</option>
-                      ${when(this.isStringColumn(col.Type), ()=>html`
-                        <option value="like">LIKE</option>
-                      `)}
-                    `)}
-                  </select>
-                </td>
-                <td>
-                  ${when(col.FKTable, ()=>html`
-                    <select id="val_${col.Name}">
-                      ${repeat(col.FKItems, (keyitem) => html`
-                        <option value="${keyitem.ID}">${keyitem.Summary}</option>
-                      `)}
-                    </select>
-                  `, ()=>html`
-                    <input type=text id="val_${col.Name}" name=${col.Name} label=${col.Name}></input>
-                  `)}
-                </td>
-              </tr>
-              `)}
-          </table>
+          <button type=button @click="${this.clear}">Clear</button>
+          <query-fields tableDesc=${JSON.stringify(this.tableDesc)}></query-fields>
         </form>
     `;
   }
